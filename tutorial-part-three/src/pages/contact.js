aafa18138ad280ee2d19768f837488e7b6094683
Saikat Das <saikatdas0790@gmail.com>
2019-05-31 13:00:54 +0530

    Create your first layout component

--- tutorial-part-three/src/pages/contact.js
+++ tutorial-part-three/src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../components/layout";
 
 export default () => (
-  <div>
+  <Layout>
     <h1>I'd love to talk! Email me at the address below</h1>
     <p>
       <a href="mailto:saikatdas0790@gmail.com">saikatdas0790@gmail.com</a>
     </p>
-  </div>
+  </Layout>
 );
