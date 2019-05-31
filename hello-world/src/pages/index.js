db7859a33d6f3e62b004bfc00ce12529f6b05676
Saikat Das <saikatdas0790@gmail.com>
2019-05-31 08:44:26 +0530

    Deploying a Gatsby Site

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/header";
 
 export default () => (
   <div style={{ color: "purple" }}>
-    <Link to="/contact/">Contact</Link>
+    <Link to="/contact">Contact</Link>
     <Header headerText="Hello Gatsby!" />
     <p>What a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
