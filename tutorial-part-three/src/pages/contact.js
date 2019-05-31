a8fa45d51e3f5aa0515179599b8fc714fc54f043
Saikat Das <saikatdas0790@gmail.com>
2019-05-31 12:56:47 +0530

    Creating layout components

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:saikatdas0790@gmail.com">saikatdas0790@gmail.com</a>
+    </p>
+  </div>
+);
