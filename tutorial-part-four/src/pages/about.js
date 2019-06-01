05ce70b972833e4b6a8ab59af4b509029f822616
Saikat Das <saikatdas0790@gmail.com>
2019-06-01 08:32:27 +0530

    Use a page query

--- tutorial-part-four/src/pages/about.js
+++ tutorial-part-four/src/pages/about.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Layout from "../components/layout";
+import { graphql } from "gatsby";
 
-export default () => (
+export default ({
+  data: {
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
   <Layout>
-    <h1>About Pandas Eating Lots</h1>
+    <h1>{title}</h1>
     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food
     </p>
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
